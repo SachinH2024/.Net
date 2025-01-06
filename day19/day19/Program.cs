4098fdf9a8929836bbef3f2e8aa175aae86059bb
SachinH <sachin.hulawale23@gmail.com>
2025-01-06 12:03:59 +0530

    day19

---
+++ day19/day19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class RecursiveName
+{
+    static void Main()
+    {
+        PrintNameRecursive("Sachin Hulawale", 10);
+    }
+
+    static void PrintNameRecursive(string name, int count)
+    {
+        if (count > 0)
+        {
+            Console.WriteLine(name);
+            PrintNameRecursive(name, count-1);
+        }
+
+
+    }
+}
