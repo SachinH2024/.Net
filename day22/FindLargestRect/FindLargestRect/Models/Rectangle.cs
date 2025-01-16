5f816a7bb6a908adbf2d3ca30fe23c65ac6570bd
SachinH <sachin.hulawale23@gmail.com>
2025-01-16 10:17:42 +0530

    day22

---
+++ day22/FindLargestRect/FindLargestRect/Models/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindLargestRect.Models
+{
+    class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public override string ToString()
+        {
+            return $"Rectangle: Width = {Width}, Height = {Height}, Area = {CalculateArea()}, Perimeter = {CalculatePerimeter()}";
+        }
+    }
+}
---
+++ day22/FindLargestRect/FindLargestRect/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FindLargestRect.Models;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle[] rectangles = new Rectangle[]
+        {
+            new Rectangle(3, 4),
+            new Rectangle(5, 2),
+            new Rectangle(1, 6),
+            new Rectangle(8, 1),
+            new Rectangle(2, 2)
+        };
+
+        // Print information about each rectangle
+        foreach (Rectangle rect in rectangles)
+        {
+            Console.WriteLine(rect);
+        }
+
+        // Find the rectangle with the largest area
+        Rectangle biggestRectangle = FindBiggestRectangle(rectangles);
+
+        Console.WriteLine("\nBiggest Rectangle:");
+        Console.WriteLine(biggestRectangle);
+
+        Console.ReadKey();
+    }
+
+    static Rectangle FindBiggestRectangle(Rectangle[] rectangles)
+    {
+        if (rectangles == null || rectangles.Length == 0)
+        {
+            return null;
+        }
+
+        Rectangle biggest = rectangles[0];
+        for (int i = 1; i < rectangles.Length; i++)
+        {
+            if (rectangles[i].CalculateArea() > biggest.CalculateArea())
+            {
+                biggest = rectangles[i];
+            }
+        }
+
+        return biggest;
+    }
+}
