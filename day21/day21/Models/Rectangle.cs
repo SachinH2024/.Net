18d5710771dd63059882b3877390cca9f5f1e836
SachinH <sachin.hulawale23@gmail.com>
2025-01-16 10:16:59 +0530

    day21

---
+++ day21/day21/Models/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day21.Models
+{
+    class Rectangle
+    {
+        public int height;
+        public int width;
+        public int CalculatedArea()
+        {
+            return height * width;
+        }
+    }
+}
