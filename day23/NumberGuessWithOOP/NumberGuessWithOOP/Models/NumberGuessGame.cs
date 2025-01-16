876d41fcd629360e3d11aed4a06ca118e3f50543
SachinH <sachin.hulawale23@gmail.com>
2025-01-16 10:18:25 +0530

    day23

---
+++ day23/NumberGuessWithOOP/NumberGuessWithOOP/Models/NumberGuessGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessWithOOP.Models
+{
+    public class NumberGuessGame
+    {
+        private int randomNumber;
+        private int attemptsRemaining;
+
+        public NumberGuessGame()
+        {
+            // Generate a random number between 1 and 100
+            Random random = new Random();
+            randomNumber = random.Next(1, 101);
+            attemptsRemaining = 3;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+            Console.WriteLine("I've chosen a random number between 1 and 100.");
+
+            while (attemptsRemaining > 0)
+            {
+                Console.Write("Enter your guess: ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("Congratulations! You guessed the number.");
+                    return;
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else
+                {
+                    Console.WriteLine("Too high!");
+                }
+
+                attemptsRemaining--;
+                Console.WriteLine($"Attempts remaining: {attemptsRemaining}");
+            }
+
+            Console.WriteLine($"You've run out of attempts. The number was {randomNumber}.");
+        }
+    }
+}
---
+++ day23/NumberGuessWithOOP/NumberGuessWithOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NumberGuessWithOOP.Models;
+
+namespace NumberGuessWithOOP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            NumberGuessGame game = new NumberGuessGame();
+            game.Play();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
