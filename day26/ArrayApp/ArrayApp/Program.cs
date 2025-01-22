403b0c6b769a76b347d78be33c93688a3d7a93b9
SachinH <sachin.hulawale23@gmail.com>
2025-01-22 10:03:57 +0530

    day26

---
+++ day26/ArrayApp/ArrayApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace ArrayApp
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "abc", "def" };
+            string[] outputArray = GenerateOutputArray(inputArray);
+
+            foreach (string row in outputArray)
+            {
+                Console.WriteLine(row);
+            }
+        }
+
+        public static string[] GenerateOutputArray(string[] inputArray)
+        {
+            int maxLength = inputArray.Length + 2;
+            string[] outputArray = new string[maxLength];
+
+            outputArray[0] = new string('*', maxLength);
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                outputArray[i + 1] = "*" + inputArray[i] + "*";
+            }
+
+            outputArray[maxLength - 1] = new string('*', maxLength);
+
+            return outputArray;
+
+        }
+    }
+}
