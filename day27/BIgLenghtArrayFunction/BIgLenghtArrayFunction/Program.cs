ad5af28dbd7e27c37c91a4e7ed8bbd373b70a709
SachinH <sachin.hulawale23@gmail.com>
2025-01-22 10:04:39 +0530

    day27

---
+++ day27/BIgLenghtArrayFunction/BIgLenghtArrayFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BIgLenghtArrayFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = { "abc", "abcd", "efgg" };
+            string[] longestArray = FindLongestString(inputArray);
+
+            Console.WriteLine(longestArray);
+            foreach (string str in longestArray)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        public static string[] FindLongestString(string[] inputArray)
+        {
+            if(inputArray==null||inputArray.Length==0)
+            {
+                return new string[0];
+            }
+            
+            int maxLength=inputArray.Max(s => s.Length);
+            return inputArray.Where(s => s.Length==maxLength).ToArray();
+
+        }
+    }
+}
---
+++ day27/BoxingUnboxingExample/BoxingUnboxingExample/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BoxingUnboxingExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // object array with integer values
+            object[] objArray = { 10, 20, 30, 40, 50 };
+
+            int sum = 0;
+
+            // unbox each object to int
+            foreach (object obj in objArray)
+            {
+                int value = (int)obj; // Unboxing
+                sum += value;
+            }
+
+            // Calculate average
+            double average = (double)sum / objArray.Length;
+
+            // Display results
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Average: " + average);
+
+            Console.ReadLine();
+        }
+    }
+}
