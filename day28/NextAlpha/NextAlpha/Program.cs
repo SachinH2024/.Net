5d59b58fd30df3a5fa27b3d5deb7cd1ddcd94b54
SachinH <sachin.hulawale23@gmail.com>
2025-01-27 09:42:53 +0530

    day28

---
+++ day28/NextAlpha/NextAlpha/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NextAlpha
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputString = "abcd";
+            string outputString = StringExtensions.NextAlphabet(inputString);
+
+            Console.WriteLine($"Input string: {inputString}");
+            Console.WriteLine($"Output string: {outputString}");
+        }
+
+        public class StringExtensions
+        {
+            public static string NextAlphabet(string input)
+            {
+                char[] chars = input.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    chars[i] = (char)(chars[i] + 1);
+                }
+                return new string(chars);
+            }
+        }
+    }
+}
