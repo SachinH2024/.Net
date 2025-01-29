66ee15a6616aa50138e3f14a1a15b5262b58ccc8
SachinH <sachin.hulawale23@gmail.com>
2025-01-29 09:54:07 +0530

    day32

---
+++ day32/ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ReverseArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputArray = { "abc", "pqr", "lmno", "sachin" };
+            string[] reversedArray = ReverseArrayInPlace(inputArray);
+
+            Console.WriteLine("Reversed Array:");
+            foreach (string str in reversedArray)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        public static string[] ReverseArrayInPlace(string[] arr)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while (left < right)
+            {   
+                // Swap elements at left and right indices 
+                string temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            return arr;
+        }
+
+    }
+}
---
+++ day32/UserDataApp/UserDataApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace UserDataApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter Address: ");
+            string address= Console.ReadLine();
+
+            string filepath = "C:\\.Net\\.Net\\day32\\UserDataApp\\Test.txt";
+
+            using (FileStream fs = new FileStream(filepath, FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs)) 
+                { 
+                    sw.WriteLine($"Username : {username}");
+                    sw.WriteLine($"Address  : {address}");
+                }
+            }
+
+            Console.WriteLine("User data saved to " + filepath);
+        }
+    }
+}
