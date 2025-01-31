a60799afc449be6dfdbd83e2bab39238cf734f69
SachinH <sachin.hulawale23@gmail.com>
2025-01-31 10:16:21 +0530

    day33

---
+++ day33/RotateArr/RotateArr/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace RotateArr
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> myList = new List<int>() { 10, 20, 30, 40 };
+
+            List<int> rotatedList=RotetElements(myList);
+
+            Console.WriteLine(string.Join(",",rotatedList));
+
+        }
+
+        public static List<int> RotetElements(List<int> lst)
+        {
+            if (lst.Count == 0)
+            {
+                return new List<int>();
+            }
+
+            List<int> rotateList = new List<int>();
+
+            for (int i = 1; i < lst.Count; i++) 
+            {
+                rotateList.Add(lst[i]);
+               
+            }
+
+            rotateList.Add(lst[0]);
+            return rotateList;
+        }
+    }
+}
