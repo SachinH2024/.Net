b7adb89f4e30cd5fef5cba4d2f1436018d853be9
SachinH <sachin.hulawale23@gmail.com>
2025-02-05 14:54:36 +0530

    day35

---
+++ day35/SplitWithoutInbuildFunction/SplitWithoutInbuildFunction/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SplitWithoutInbuildFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] urls = { "abc.edu", "pqr.com", "lmno.org" };
+            string[] extensions = GetExtensions(urls);
+            foreach (string extension in extensions)
+            {
+                Console.WriteLine(extension);
+            }
+        }
+
+
+        public static string GetExtension(string url)
+        {
+            int index = url.LastIndexOf('.');
+            if (index == -1)
+            {
+                return string.Empty;
+            }
+            return url.Substring(index + 1);
+        }
+
+        public static string[] GetExtensions(string[] urls)
+        {
+            string[] extensions = new string[urls.Length];
+            for (int i = 0; i < urls.Length; i++)
+            {
+                extensions[i] = GetExtension(urls[i]);
+            }
+            return extensions;
+        }
+
+
+
+        }
+    }
+
