351e5c0f0a05f9d6143b297bc4cabf019f998d22
SachinH <sachin.hulawale23@gmail.com>
2025-02-07 12:22:19 +0530

    day38

---
+++ day38/Deserilization/Deserilization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Deserilization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(@"C:\.Net\.Net\day38\cust1.json");
+            var jsonString = sr.ReadToEnd();
+
+            Console.WriteLine(jsonString);
+
+            var customer = JsonSerializer.Deserialize<Customer>(jsonString);
+
+            Console.WriteLine("Dserialized customer Details :");
+            Console.WriteLine("Customer ID:" + customer.Id);
+            Console.WriteLine("Customer Name:" + customer.Name);
+            Console.WriteLine();
+            Console.WriteLine("Customer Address");
+            foreach (Address address in customer.Addresses)
+            {
+                Console.WriteLine($"- City: {address.City}, Country: {address.Country}");
+            }
+
+        }
+    }
+}
---
+++ day38/XMLSerialization/XMLSerialization/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                Name = "Sachin",
+                Addresses = new List<Address>
+            {
+                new Address { City = "Chennai", Country = "India" },
+                new Address { City = "Mumbai", Country = "India" }
+            }
+            };
+
+            // Serialization
+            XmlSerializer serializer = new XmlSerializer(typeof(Customer));
+            using (TextWriter writer = new StringWriter()) 
+            {
+                serializer.Serialize(writer, customer);
+                string xmlOutput = writer.ToString();
+                Console.WriteLine("Serialized XML:");
+                Console.WriteLine(xmlOutput);
+
+                 
+                 using (StreamWriter file = new StreamWriter("customer.xml"))
+                 {
+                     serializer.Serialize(file, customer);
+                 }
+
+            }
+
+           
+        }
+    }
+}
