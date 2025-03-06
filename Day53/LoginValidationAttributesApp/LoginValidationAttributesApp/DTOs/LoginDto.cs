06d0375b77eb6b666266482493f27e45f8e90efd
SachinH <sachin.hulawale23@gmail.com>
2025-03-06 15:22:58 +0530

    day53

---
+++ Day53/LoginValidationAttributesApp/LoginValidationAttributesApp/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LoginValidationAttributesApp.CustomAttributes;
+
+namespace LoginValidationAttributesApp.DTOs
+{
+    public class LoginDto
+    {
+        [CustomUserNameAttribute]
+        public string UserName { get; set; }
+
+        [CustomerUserPassword]
+        public string UserPassword { get; set; }
+    }
+}
