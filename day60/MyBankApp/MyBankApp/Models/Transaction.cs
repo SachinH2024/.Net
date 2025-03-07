ea2afc9fb34316eea077ef5d6cc78c9b511afff0
SachinH <sachin.hulawale23@gmail.com>
2025-03-07 11:03:07 +0530

    day60

---
+++ day60/MyBankApp/MyBankApp/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBankApp.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Debit { get; set; }
+        public decimal Credit { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int UserId { get; set; } // Foreign key to User
+        //public User User { get; set; } // Navigation property
+    }
+}
---
+++ day60/MyBankApp/MyBankApp/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBankApp.Models
+{
+    public class User
+    {
+        [Key]
+        //public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; } 
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        
+
+        //[EmailAddress(ErrorMessage = "Invalid email address.")]
+        //public string Email { get; set; } //Optional for signup
+
+        //public string? Name { get; set; }
+    }
+}
